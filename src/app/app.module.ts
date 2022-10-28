d5389e0b34035d80d92caed233981af3a9d6d495
bryanchristopher <bryanchristttt@gmail.com>
2022-10-28 20:41:28 +0700

    uts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    EcommerceComponent
   ],
   imports: [
     BrowserModule
